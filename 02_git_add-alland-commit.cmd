b935dc47c8425f66e39be87964a5d93ed3cad8bb
PyraN-Ranjith Jayawardana <pyranranjith@gmail.com>
2024-02-24 13:00:08 +0530

    03. . cmd added

--- 02_git_add-alland-commit.cmd
+++ 02_git_add-alland-commit.cmd
@@ -1,9 +1,8 @@
 @echo off
 ::___________Set Variables______________________
 :: Origin for this repo
-::  set this_repo=https://github.com/Pyran-Ranjith/Global.git
 :: Commit msg
-    set commit_msg="changed a lot"
+    set commit_msg="03. . cmd added"
 ::____________Process begin here________________________
 ::echo Hear is the result_______________________
 git add .
